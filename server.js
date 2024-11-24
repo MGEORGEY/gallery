3018cde69addf1ea4183aede6e86bbdef2d2557c
MUTWIRI GEORGE <mgeorgey@MUTWIRIs.local>
2024-11-24 21:38:17 +0300

    Removed duplicate requests for a port

--- server.js
+++ server.js
@@ -56,10 +56,8 @@ const image = require('./routes/image');
 app.use('/', index);
 app.use('/image', image);
 
-const PORT = process.env.PORT ;
-app.listen(PORT, () => {
-    console.log(`Server is listening at http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT,() =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
 });
